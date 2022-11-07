6c71b1c92778d8c48bc1ba624f4da904569773f4
Rebolon <richard.tribes@gmail.com>
2022-11-07 23:01:54 +0100

    routing with hash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })], // or use prerender to generate real urls (but incompatible with github pages
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
